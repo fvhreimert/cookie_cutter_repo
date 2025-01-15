8f56d9f40b78caf7b499e286351d324a56121303
frederikreimert <frederikreimert@gmail.com>
2025-01-15 15:10:00 +0100

    new trainin 23

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,6 @@
 import os
 
-_TEST_ROOT = os.path.dirname(__file__)  # root of test folder
-_PROJECT_ROOT = os.path.dirname(_TEST_ROOT)  # root of project
+# Dynamically determine project root directory
+_PROJECT_ROOT = Path(__file__).resolve().parents[1]  # Adjust to match project structure
+_TEST_ROOT = os.path.join(_PROJECT_ROOT, "tests/")  # root of test folder
 _PATH_DATA = os.path.join(_PROJECT_ROOT, "data/", "processed/")  # root of data
